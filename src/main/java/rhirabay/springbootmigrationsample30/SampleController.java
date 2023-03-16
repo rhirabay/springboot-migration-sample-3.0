595c73b8a766c4ca8da75c714f8ba98395cd2bc4
rhirabay <rhirabay@yahoo-corp.jp>
2023-03-17 00:10:24 +0900

    migrate springboot to 3.0 from 2.7. [shepherd]

--- src/main/java/rhirabay/springbootmigrationsample30/SampleController.java
+++ src/main/java/rhirabay/springbootmigrationsample30/SampleController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.constraints.Max;
+import jakarta.validation.constraints.Max;
 
 @RestController
 public class SampleController {
