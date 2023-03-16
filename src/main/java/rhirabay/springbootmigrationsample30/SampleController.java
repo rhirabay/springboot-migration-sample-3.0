8a8ef624e4c6e27b7f9dff13bf5aae96179ac3fb
rhirabay <rhirabay@yahoo-corp.jp>
2023-03-16 21:07:07 +0900

    first commit

---
+++ src/main/java/rhirabay/springbootmigrationsample30/SampleController.java
@@ -0,0 +1,17 @@
+package rhirabay.springbootmigrationsample30;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.Max;
+
+@RestController
+public class SampleController {
+    @GetMapping({"/sample", "/hello"})
+    public String hello(
+            @RequestParam @Max(100) int num
+    ) {
+        return "hello";
+    }
+}
